a262dc9722aeab12f5b67fce3d3fbba5a6061ead
varundeepakgudhe <varundeepakgudhe@users.noreply.github.com>
2024-12-18 01:22:41 +0000

    deploy: 9a7627b1dbd1b452a5e3bcd7626ab29da344b2d3

---
+++ app.js
@@ -0,0 +1,27 @@
+document.getElementById('addition-form').addEventListener('submit', async (event) => {
+    event.preventDefault();
+  
+    const num1 = document.getElementById('num1').value;
+    const num2 = document.getElementById('num2').value;
+  
+    // const response = await fetch('https://powerful-spire-73471-ef9468f77a19.herokuapp.com/add', {
+    //     method: 'POST',
+    //     headers: { 'Content-Type': 'application/json' },
+    //     body: JSON.stringify({ num1, num2 }),
+    //   });
+    const response = await fetch('http://127.0.0.1:5000/add', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ num1, num2 }),
+      }); 
+  
+    const result = await response.json();
+    if (response.ok) {
+      document.getElementById('result').textContent = `Result: ${result.result}`;
+    } else {
+      document.getElementById('result').textContent = `Error: ${result.error}`;
+    }
+  });
+  
